949231c9a5cf7370011d6ee8b057cf633b3dc486
martin <martinberon02@gmail.com>
2025-06-04 18:35:26 -0300

    Actualizacion index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import bookRoutes from './routes/books';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(cors())
 
-connectDB();
+
 
 app.use(express.json());
 app.use('/api', bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
